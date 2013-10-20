83b4d32788fcf98a49a3815829159db30f5818f0
addisonweiler <alweiler@stanford.edu>
2013-10-20 16:20:56 -0700

    Milestone 1 - 3 features (Age, state, gender) gives us a 5-fold validation acc. of around 80% using Naive Bayes.

--- patient.py
+++ patient.py
@@ -4,11 +4,10 @@ from sklearn.naive_bayes import GaussianNB
 states = {
 	'WA':0, 'WI':1, 'WV':2, 'FL':3, 'WY':4, 'NH':5, 'NJ':6, 'NM':7,
 	'NC':8, 'ND':9, 'NE':10, 'NY':11, 'RI':12, 'NV':13, 'CO':14, 'CA':15,
-	'GA':16, 'CT':17, 'OK':18, 'OH':19, 'KS':20,
-	'SC':21,'KY':22,'OR':23,'SD':24,'DE':25,'HI':26,'TX':27,'LA':28,'TN':29,
-	'PA':30,'VA':31,'AK':32,'AL':33,'AR':34,'VT':35,'IL':36,'IN':37,'IA':38,
-	'AZ':39,'ID':40,'ME':41,'MD':42,'MA':43,'UT':44,'MO':45,'MN':46,'MI':47,
-	'MT':48,'MS':49,'PR':50,'DC':51
+	'GA':16, 'CT':17, 'OK':18, 'OH':19, 'KS':20, 'SC':21,'KY':22,'OR':23,
+	'SD':24,'DE':25,'HI':26,'TX':27,'LA':28,'TN':29, 'PA':30,'VA':31,'AK':32,
+	'AL':33,'AR':34,'VT':35,'IL':36,'IN':37,'IA':38,'AZ':39,'ID':40,'ME':41,
+	'MD':42,'MA':43,'UT':44,'MO':45,'MN':46,'MI':47,'MT':48,'MS':49,'PR':50,'DC':51
 }
 
 class Patient:
@@ -25,7 +24,6 @@ class Patient:
 			self.DoctorGuid = row[5]
 			
 			self.featureVector = [self.gender, self.year_of_birth, states[self.state]]
-			#self.featureVector = [self.gender, self.year_of_birth, self.state]
 		if data == "TEST":
 			if row[1] == "M": self.gender = 1
 			else: self.gender = 0
@@ -56,30 +54,11 @@ def train():
 			Y.append(p.hasDiabetes)
 	return (X, Y)
 
-"""
-def test(model):
-	X = [] #Feature Vectors
-	with open('test.csv', 'r+') as csvfile:
-		reader = csv.reader(csvfile)
-		next(reader)
-		for row in reader:
-			p = Patient(row, "TEST")
-			X.append(p.featureVector)
-	count0 = 0
-	count1 = 0
-	for i in X:
-		if model.predict(i) == 0: count0 += 1
-		else: count1 += 1
-	print "No diabetes: " + str(count0)
-	print "Has diabetes: " + str(count1)
-"""
-
 FOLD_COUNT = 50
 
 def crossFoldValidation(X, Y):
 	model = GaussianNB()
 	correctArr = []
-	#incorrectArr = []
 	section = len(X) / FOLD_COUNT
 	for j in range(0, FOLD_COUNT):
 		newX = X[0:j*section] + X[(j+1)*section+1:]
@@ -89,15 +68,13 @@ def crossFoldValidation(X, Y):
 		testY = Y[j*section: (j+1)*section+1]
 		
 		model.fit(newX, newY)
-		
-		Correct = 0
 		Incorrect = 0
+		Correct = 0
 		for i in range(len(testX)):
 			if model.predict(testX[i]) == testY[i]: Correct += 1
 			else: Incorrect += 1
 		accuracy = float(Correct) / (Correct + Incorrect)
 		correctArr.append(accuracy)
-		#incorrectArr.append(float(Incorrect) / (Correct + Incorrect))
 		print "Run " + str(j+1) + ": " + str(accuracy)
 	
 	print "Average Accuracy: " + str(float(sum(correctArr)) / FOLD_COUNT)
