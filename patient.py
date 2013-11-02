d80c53bf774d83ea8ff38fea2b577f78e0430192
addisonweiler <alweiler@stanford.edu>
2013-11-02 15:26:18 -0700

    Patient Modification, now has grouping for features

--- patient.py
+++ patient.py
@@ -1,41 +1,58 @@
 import csv
-from sklearn.naive_bayes import GaussianNB
 
-states = {
-	'WA':0, 'WI':1, 'WV':2, 'FL':3, 'WY':4, 'NH':5, 'NJ':6, 'NM':7,
-	'NC':8, 'ND':9, 'NE':10, 'NY':11, 'RI':12, 'NV':13, 'CO':14, 'CA':15,
-	'GA':16, 'CT':17, 'OK':18, 'OH':19, 'KS':20, 'SC':21,'KY':22,'OR':23,
-	'SD':24,'DE':25,'HI':26,'TX':27,'LA':28,'TN':29, 'PA':30,'VA':31,'AK':32,
-	'AL':33,'AR':34,'VT':35,'IL':36,'IN':37,'IA':38,'AZ':39,'ID':40,'ME':41,
-	'MD':42,'MA':43,'UT':44,'MO':45,'MN':46,'MI':47,'MT':48,'MS':49,'PR':50,'DC':51
-}
+#Gets the age group by decade a patient was born in
+def AgeGroup(year_of_birth):
+	dec = year_of_birth - 1900
+	if dec < 10: return 0
+	if dec < 20: return 1
+	if dec < 30: return 2
+	if dec < 40: return 3
+	if dec < 50: return 4
+	if dec < 60: return 5
+	if dec < 70: return 6
+	if dec < 80: return 7
+	if dec < 90: return 8
+	if dec < 100: return 9
+
+WEST = ['WA', 'OR', 'WY', 'MT', 'ID', 'CO', 'UT', 'NV', 'AZ', 'CA','NM', 'AK', 'HI']
+MIDWEST = ['ND', 'SD', 'MN', 'WI', 'MI', 'NE', 'IA', 'IL', 'IN', 'OH', 'KS', 'MO']
+SOUTH = ['TX', 'OK', 'AR', 'LA', 'WV', 'MD', 'DE', 'DC', 'KY', 'VA', 'TN', 'NC', 'MS', 'AL', 'GA', 'SC', 'FL', 'PR']
+NORTHEAST = ['ME', 'NH', 'VT', 'NY', 'MA', 'RI', 'CT', 'NJ', 'PA']
+
+def US_Region(state):
+	if state in WEST: return 0
+	if state in MIDWEST: return 1
+	if state in SOUTH: return 2
+	if state in NORTHEAST: return 3
+	raise Exception(state + " not found in any region")
 
 class Patient:
-	def __init__(self, csvfileReaderRow, data):	
+	def __init__(self, csvfileReaderRow):	
 		row = csvfileReaderRow
 		self.PatientGuid = row[0]
-		if data == "TRAIN":
-			self.hasDiabetes = int(row[1])
-			if row[2] == "M": self.gender = 1
-			else: self.gender = 0
-			self.year_of_birth = int(row[3])
-			
-			self.state = row[4]
-			self.DoctorGuid = row[5]
-			
-			self.featureVector = [self.gender, self.year_of_birth, states[self.state]]
-		if data == "TEST":
-			if row[1] == "M": self.gender = 1
-			else: self.gender = 0
-			self.year_of_birth = int(row[2])
-			self.state = row[3]
-			self.DoctorGuid = row[4]
-			self.featureVector = [self.gender, self.year_of_birth, states[self.state]]
+		self.DoctorGuid = row[5]
 		
-	
+		self.featureVector = []
+		
+		#Do they have diabetes
+		self.hasDiabetes = (int(row[1]))
+		
+		#Gender Test, 1 for Male, 0 for female
+		if row[2] == "M": self.featureVector.append(1)
+		else: self.featureVector.append(0)
+		
+		#Age test, see AgeGroup for details
+		self.featureVector.append(AgeGroup(int(row[3])))
+		
+		#Region test, see US_Region for details
+		self.featureVector.append(US_Region(row[4]))
+
 	def featureVector(self):
 		return self.featureVector
 
+	def classification():
+		return self.hasDiabetes
+	
 
 def train():
 	X = [] #Feature Vectors
@@ -43,43 +60,15 @@ def train():
 	with open('trainingSet/training_SyncPatient.csv', 'r+') as csvfile:
 		reader = csv.reader(csvfile)
 		next(reader)
-		mDiabetic = 0
-		mNon = 0
-		fDiabetic = 0
-		fNon = 0
 		
+		#Create the patients
 		for row in reader:
-			p = Patient(row, "TRAIN")
+			p = Patient(row)
+			print p.featureVector
 			X.append(p.featureVector)
 			Y.append(p.hasDiabetes)
 	return (X, Y)
 
 FOLD_COUNT = 50
 
-def crossFoldValidation(X, Y):
-	model = GaussianNB()
-	correctArr = []
-	section = len(X) / FOLD_COUNT
-	for j in range(0, FOLD_COUNT):
-		newX = X[0:j*section] + X[(j+1)*section+1:]
-		newY = Y[0:j*section] + Y[(j+1)*section+1:]
-	
-		testX = X[j*section: (j+1)*section+1]
-		testY = Y[j*section: (j+1)*section+1]
-		
-		model.fit(newX, newY)
-		Incorrect = 0
-		Correct = 0
-		for i in range(len(testX)):
-			if model.predict(testX[i]) == testY[i]: Correct += 1
-			else: Incorrect += 1
-		accuracy = float(Correct) / (Correct + Incorrect)
-		correctArr.append(accuracy)
-		print "Run " + str(j+1) + ": " + str(accuracy)
-	
-	print "Average Accuracy: " + str(float(sum(correctArr)) / FOLD_COUNT)
-	
-t = train()
-crossFoldValidation(t[0], t[1])
-#test(model)
-
+train()
